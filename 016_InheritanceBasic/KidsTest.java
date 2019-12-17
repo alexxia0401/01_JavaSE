f059f97395b32fe76262f1d64037948fb9a06ce1
Qing Xia <alexxia@outlook.com>
2019-12-17 14:51:37 +0800

    a basic Java inheritance example

---
+++ 016_InheritanceBasic/KidsTest.java
@@ -0,0 +1,12 @@
+public class KidsTest {
+    public static void main(String[] args) {
+        Kids someKid = new Kids(12);
+        
+        someKid.printAge();
+        someKid.setSalary(0);
+        someKid.setSex(1);
+        
+        someKid.employeed();
+        someKid.manOrWoman();
+    }
+}
